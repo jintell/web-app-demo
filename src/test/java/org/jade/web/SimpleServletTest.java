2c73a9eb1de1f25c6f788c06d7dd1200510f0220
jintell <josleke@gmail.com>
2019-12-27 20:38:23 +0000

    Updated source and target to java 1.8 and added SimpleServlet unit test class

---
+++ src/test/java/org/jade/web/SimpleServletTest.java
@@ -0,0 +1,21 @@
+package org.jade.web;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+ 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class SimpleServletTest {
+
+	@Test
+	public void valid_content_test(){
+		System.out.println("This is unit test on the SimpleServlet");
+		assertTrue(true);
+	}
+
+}
